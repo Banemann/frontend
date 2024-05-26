ca37f9d2c40f5302614c8b017b71fc8b07c67f92
Casper Banemann <casperbanemann@gmail.com>
2024-05-27 00:04:03 +0200

    bands forside

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,43 @@
-
+"use client";
+import { useEffect, useState } from 'react';
 import Footer from "./components/Footer";
 import styles from "./styles.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Home() {
+function BandPage() {
+  const [bandInfo, setBandInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBandInfo = async (maxItems = 3) => {
+      try {
+        const response = await fetch('https://sepia-bow-age.glitch.me/bands/');
+        if (!response.ok) {
+          throw new Error(`Fetch error: ${response.statusText}`);
+        }
+        const data = await response.json();
+        const limitedData = data.slice(0, maxItems);
+        setBandInfo(limitedData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchBandInfo();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Fejl ved loading af band information {error.message}</p>;
+
   return (
     <main className={styles.contentContainer}>
       <div className={styles.contentBox}>
-        
         <div className={styles.homeHeader}>
-     
-        <h1 className={styles.title}>LAVAFEST</h1>
+          <h1 className={styles.title}>LAVAFEST</h1>
         </div>
         
         <nav className={styles.nav}>
@@ -35,19 +61,18 @@ export default function Home() {
           </ul>
         </nav>
 
-        
         <div className={styles.bandBox}>
-          
-          <div className={styles.bandCard}>
-            <h2>Band 1</h2>
-            <Image src="/images/1.jpg" alt="Band Image" width={600} height={300} />
-          </div>
-
-          </div>
-        
-
+          {bandInfo && bandInfo.map(band => (
+            <div key={band.slug} className={styles.bandCard}>
+              <h2>{band.name}</h2>
+              <Image src={`/logos/${band.logo}`} alt={band.name} width={200} height={200} />
+            </div>
+          ))}
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </main>
   );
 }
+
+export default BandPage;
