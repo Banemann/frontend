7419e30f77a85eddd41073c6e9d4546e0ed1ccb6
Casper Banemann <casperbanemann@gmail.com>
2024-05-27 17:41:54 +0200

    header +title+allebands

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import Image from 'next/image';
 import styles from './Header.module.css';
 
 const Header = () => {
@@ -9,22 +8,21 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.navContainer}>
-        <Link className={styles.logo} href="/" passHref>
-          <Image priority src="/logos/terminalist.jpg" alt="Logo" width={50} height={50} />
+        <Link className={styles.title} href="/" passHref>
+          LAVAFEST
         </Link>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
-           <li className={`${styles.link} ${router.pathname === '/billetter' ? styles.active : ''}`}>
+            <li className={`${styles.link} ${router.pathname === '/billetter' ? styles.active : ''}`}>
               <Link prefetch={false} href="/billetter">
                 Billetter
               </Link>
             </li>
-             <li className={`${styles.link} ${router.pathname === '/program' ? styles.active : ''}`}>
+            <li className={`${styles.link} ${router.pathname === '/program' ? styles.active : ''}`}>
               <Link prefetch={false} href="/program">
                 Program
               </Link>
             </li>
-
             <li className={`${styles.link} ${router.pathname === '/allbands' ? styles.active : ''}`}>
               <Link prefetch={false} href="/allbands">
                 Bands
